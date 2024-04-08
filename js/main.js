f81899f40864ad67ec02a23f8534c408a86f872e
kartik-barman <mrkartik1st@gmail.com>
2024-04-08 19:32:25 +0530

    Uluberia Government polytecnic

--- js/main.js
+++ js/main.js
@@ -36,3 +36,48 @@ crossOutline.addEventListener("click", ()=>{
   crossOutline.style.display = "none";
 
 })
+
+const mark = document.querySelector(".mark")
+const start = document.querySelector(".startBtn");
+const stop = document.querySelector(".stopBtn");
+
+start.onclick = startMark;
+stop.onclick = stopMark;
+function startMark(){
+  mark.start();
+}
+function stopMark(){
+  mark.stop();
+}
+console.log(mark);
+
+
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+  autoplay: {
+    delay: 3000, // Automatically slide every 5 seconds
+    disableOnInteraction: false, // Continue sliding even when user interacts with slider
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
